7a6b8c565053f54be7fdff6991601adb7d026a6f
Jaime Serrano <serranojaime41@gmail.com>
2022-12-13 19:52:56 -0600

    Learning about rectangles

--- main.py
+++ main.py
@@ -8,9 +8,15 @@ pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
 test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 
-sky_surface = pygame.image.load('graphics/Sky.png')
-ground_surface = pygame.image.load('graphics/ground.png')
+sky_surface = pygame.image.load('graphics/Sky.png').convert()
+ground_surface = pygame.image.load('graphics/ground.png').convert()
 test_surface = test_font.render('My Game ', False, 'Black')
+
+snail_surf = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
+snail_rect = snail_surf.get_rect(midbottom = (600,300))
+
+player_surf = pygame.image.load('graphics/player/player_walk_1.png').convert_alpha()
+player_rect = player_surf.get_rect(midbottom = (80,300))
 
 while True :
     for event in pygame.event.get() :
@@ -21,6 +27,15 @@ while True :
     screen.blit(sky_surface, (0,0))
     screen.blit(ground_surface, (0,300))
     screen.blit(test_surface, (300, 50))
+
+    snail_rect.x -= 4
+    if snail_rect.right <= 0 :
+        snail_rect.left = 800
+
+    screen.blit(snail_surf, snail_rect)
+    
+    screen.blit(player_surf, player_rect)
+
     
 
     pygame.display.update()
