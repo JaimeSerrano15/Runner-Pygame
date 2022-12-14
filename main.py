65894c053bdb7d890812661658d97bef0d900528
Jaime Serrano <serranojaime41@gmail.com>
2022-12-13 20:20:04 -0600

    Some silly jumps

--- main.py
+++ main.py
@@ -19,14 +19,19 @@ snail_rect = snail_surf.get_rect(midbottom = (600,300))
 
 player_surf = pygame.image.load('graphics/player/player_walk_1.png').convert_alpha()
 player_rect = player_surf.get_rect(midbottom = (80,300))
+player_gravity = 0
 
 while True :
     for event in pygame.event.get() :
         if event.type == pygame.QUIT :
             pygame.quit()
             exit()
-        if event.type == pygame.MOUSEMOTION :
-            if player_rect.collidepoint(event.pos) : print('collision')
+        if event.type == pygame.KEYDOWN :
+            if event.key == pygame.K_SPACE :
+                player_gravity = -20
+        if event.type == pygame.MOUSEBUTTONDOWN :
+            if player_rect.collidepoint(event.pos) : 
+                player_gravity = -20
 
     screen.blit(sky_surface, (0,0))
     screen.blit(ground_surface, (0,300))
@@ -40,7 +45,15 @@ while True :
         snail_rect.left = 800
 
     screen.blit(snail_surf, snail_rect)
+
+    # Player
+    player_gravity += 1
+    player_rect.y += player_gravity
     screen.blit(player_surf, player_rect)
+
+    #keys = pygame.key.get_pressed()
+    #if keys[pygame.K_SPACE] :
+    #    print('Jump')
 
     #if player_rect.colliderect(snail_rect) :
     #    print('collision')
