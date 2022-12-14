2bade8aaf142bdb9fe5daa8decc14e8319a6331e
Jaime Serrano <serranojaime41@gmail.com>
2022-12-13 19:28:23 -0600

    Addign some surfaces

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ pygame.init()
 screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
+test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
+
+sky_surface = pygame.image.load('graphics/Sky.png')
+ground_surface = pygame.image.load('graphics/ground.png')
+test_surface = test_font.render('My Game ', False, 'Black')
 
 while True :
     for event in pygame.event.get() :
@@ -13,8 +18,10 @@ while True :
             pygame.quit()
             exit()
 
-    # Draw all our elements
+    screen.blit(sky_surface, (0,0))
+    screen.blit(ground_surface, (0,300))
+    screen.blit(test_surface, (300, 50))
+    
 
-    # Update everything
     pygame.display.update()
     clock.tick(60)
