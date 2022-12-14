8cd52caaf39c3516af293b1c52a8db79370b58f7
Jaime Serrano <serranojaime41@gmail.com>
2022-12-13 20:09:06 -0600

    Some color and Collisions

--- main.py
+++ main.py
@@ -6,11 +6,13 @@ pygame.init()
 screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
-test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
+text_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 
 sky_surface = pygame.image.load('graphics/Sky.png').convert()
 ground_surface = pygame.image.load('graphics/ground.png').convert()
-test_surface = test_font.render('My Game ', False, 'Black')
+
+score_surf = text_font.render('My Game ', False, (64,64,64))
+score_rect = score_surf.get_rect(center = (400,50))
 
 snail_surf = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
 snail_rect = snail_surf.get_rect(midbottom = (600,300))
@@ -23,19 +25,29 @@ while True :
         if event.type == pygame.QUIT :
             pygame.quit()
             exit()
+        if event.type == pygame.MOUSEMOTION :
+            if player_rect.collidepoint(event.pos) : print('collision')
 
     screen.blit(sky_surface, (0,0))
     screen.blit(ground_surface, (0,300))
-    screen.blit(test_surface, (300, 50))
+
+    pygame.draw.rect(screen, '#c0e8ec', score_rect)
+    pygame.draw.rect(screen, '#c0e8ec', score_rect, 10)
+    screen.blit(score_surf, score_rect)
 
     snail_rect.x -= 4
     if snail_rect.right <= 0 :
         snail_rect.left = 800
 
     screen.blit(snail_surf, snail_rect)
-    
     screen.blit(player_surf, player_rect)
 
+    #if player_rect.colliderect(snail_rect) :
+    #    print('collision')
+    
+    #mouse_pos = pygame.mouse.get_pos()
+    #if player_rect.collidepoint((mouse_pos)) :
+    #    print('collision')
     
 
     pygame.display.update()
